836bd6b029631090eda45be3408bff85500c77c3
Leonardo <leo.bonfim.95@gmail.com>
2020-12-20 16:43:51 -0300

    erro no styled components

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const Signin: React.FC = () => {
+  return <View />;
+};
+
+export default Signin;
